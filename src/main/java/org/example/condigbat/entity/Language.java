b8583665b9cf853b642c51c9e183669f83308f84
Yusufbek04 <yusufbekgold1@gmail.com>
2022-08-16 00:57:44 +0500

    coding bat crud

---
+++ src/main/java/org/example/condigbat/entity/Language.java
@@ -0,0 +1,19 @@
+package org.example.condigbat.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.example.condigbat.entity.template.AbsTitleIntegerEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"title"}))
+public class Language extends AbsTitleIntegerEntity {
+
+    @Column(unique = true, nullable = false)
+    private String url;
+}
